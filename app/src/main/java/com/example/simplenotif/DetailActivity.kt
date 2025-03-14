9058e68f013ae5ca95f0de8d49eab24893ed4e25
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-14 13:04:14 +0700

    push

--- app/src/main/java/com/example/simplenotif/DetailActivity.kt
+++ app/src/main/java/com/example/simplenotif/DetailActivity.kt
@@ -1,14 +1,24 @@
 package com.example.simplenotif
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.simplenotif.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    companion object{
+        const val EXTRE_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRE_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
 
     }
 }
